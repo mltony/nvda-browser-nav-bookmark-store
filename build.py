abbe4bdc59c9efe089afd33a4b3ff197b7d18073
mltony <anton.malykh+0@gmail.com>
2024-12-31 16:36:00 -0800

    Initial commit

---
+++ build.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import os,re,sys
+import json
+
+
+websites = {}
+currentDir = os.path.dirname(os.path.abspath(__file__))
+extension = '.browsernav-website'
+outputFile = os.path.join(currentDir, 'output', 'websites.json')
+for fileName in os.listdir(currentDir):
+    if not fileName.endswith(extension):
+        continue
+    j = json.loads(open(fileName, 'r', encoding='utf-8').read())
+    name = j['name']
+    if fileName != f"{name}{extension}":
+        print(f"Build error: file {fileName} contains website definition with a different name {name}. Please rename either the file or website fefinition so that they match. ")
+        sys.exit(1)
+    websites[name] = {
+        'website': j,
+    }
+
+f = open(outputFile, 'w', encoding='utf-8')
+try:
+    s = json.dumps(websites, indent=4, sort_keys=True)
+    print(s, file=f)
+finally:
+    f.close()
+
+print("Successfully built websites in local repository ! Now please create a PR to submit your change.")
